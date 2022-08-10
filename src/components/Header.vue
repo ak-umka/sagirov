2130ad0c0cd67783045ece1d9ae0d090d1d3a6de
ak-umka <umbet70@gmail.com>
2022-08-10 20:31:07 +0600

    fix grid main page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+  <nav class="lg:px-6 py-10 dark:bg-gray-800">
     <div
       class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl"
     >
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,43 +2,49 @@
 
 <template>
   <div class="home mx-auto max-w-screen-xl">
-    <div class="flex justify-between">
-      <div>
-        <h1 class="text-white text-5xl uppercase">Путешествие</h1>
-        <h2 class="text-white text-3xl">на красную планету</h2>
-      </div>
-      <div class="grid grid-cols-2 gap-2">
-        <div
-          class="flex flex-col justify-center text-center items-center box-content h-48 w-48 p-4 bg-gradient-to-br from-transparent via-transparent to-slate-50/10"
-        >
-          <h1 class="text-white text-xl">мы</h1>
-          <h1 class="text-white text-5xl">1</h1>
-          <h1 class="text-white text-xl">на рынке</h1>
+    <div class="box-content">
+      <div class="flex justify-between">
+        <div class="pt-36">
+          <h1
+            class="tracking-wide text-transparent bg-clip-text bg-gradient-to-r from-white via-orange-300 to-orange-700 text-6xl uppercase"
+          >
+            Путешествие
+          </h1>
+          <h2 class="text-white text-3xl">на красную планету</h2>
         </div>
-        <div
-          class="flex flex-col justify-center text-center items-center box-content h-48 w-48 p-4 bg-gradient-to-bl from-transparent via-transparent to-slate-50/10"
-        >
-          <h1 class="text-white text-xl">гарантируем</h1>
-          <h1 class="text-white text-5xl">50%</h1>
-          <h1 class="text-white text-xl">безопаность</h1>
-        </div>
-        <div
-          class="flex flex-col justify-center text-center items-center box-content h-48 w-48 p-4 bg-gradient-to-tr from-transparent via-transparent to-slate-50/10"
-        >
-          <h1 class="text-white text-xl">календарик за</h1>
-          <h1 class="text-white text-5xl">2001</h1>
-          <h1 class="text-white text-xl">в подарок</h1>
-        </div>
-        <div
-          class="flex flex-col justify-center text-center items-center box-content h-48 w-48 p-4 bg-gradient-to-tl from-transparent via-transparent to-slate-50/10"
-        >
-          <h1 class="text-white text-xl">путшествие</h1>
-          <h1 class="text-white text-5xl">597</h1>
-          <h1 class="text-white text-xl">дней</h1>
+        <div class="grid grid-cols-2 gap-2 pt-20">
+          <div
+            class="flex flex-col justify-center text-center items-center box-content h-36 w-36 p-4 bg-gradient-to-br from-transparent via-transparent to-slate-50/10"
+          >
+            <h1 class="text-slate-50/40 text-xl pb-3">мы</h1>
+            <h1 class="text-white text-5xl">1</h1>
+            <h1 class="text-slate-50/40 text-xl">на рынке</h1>
+          </div>
+          <div
+            class="flex flex-col justify-center text-center items-center box-content h-36 w-36 p-4 bg-gradient-to-bl from-transparent via-transparent to-slate-50/10"
+          >
+            <h1 class="text-slate-50/40 text-xl pb-3">гарантируем</h1>
+            <h1 class="text-white text-5xl">50%</h1>
+            <h1 class="text-slate-50/40 text-xl">безопаность</h1>
+          </div>
+          <div
+            class="flex flex-col justify-center text-center items-center box-content h-36 w-36 p-4 bg-gradient-to-tr from-transparent via-transparent to-slate-50/10"
+          >
+            <h1 class="text-slate-50/40 text-xl pb-3">календарик за</h1>
+            <h1 class="text-white text-5xl">2001</h1>
+            <h1 class="text-slate-50/40 text-xl">в подарок</h1>
+          </div>
+          <div
+            class="flex flex-col justify-center text-center items-center box-content h-36 w-36 p-4 bg-gradient-to-tl from-transparent via-transparent to-slate-50/10"
+          >
+            <h1 class="text-slate-50/40 text-xl pb-3">путшествие</h1>
+            <h1 class="text-white text-5xl">597</h1>
+            <h1 class="text-slate-50/40 text-xl">дней</h1>
+          </div>
         </div>
       </div>
     </div>
-    <button class="text-white">Начать путшествие</button>
+    <button class="p-2 border-4 border-b-indigo-500   text-white">Начать путшествие</button>
   </div>
 </template>
 
